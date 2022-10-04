bfa51331f341b38b5c41d7774674813b06afdad8
Sara Choi <sarachoi10@hotmail.com>
2022-10-04 05:46:44 +0000

    Completed meme gallery project

---
+++ main.js
@@ -0,0 +1,29 @@
+const $gallery = document.querySelector('main');
+
+const imageUrls = [
+  'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
+  'https://c.tenor.com/uYP_Nkq8VPsAAAAd/coding-hello-world.gif',
+  'https://wiki.godvillegame.com/images/thumb/7/76/Prap.jpg/298px-Prap.jpg'
+];
+const $imageUrlInput = document.querySelector('input');
+const $addImageButton = document.querySelector('button');
+
+$addImageButton.addEventListener('click', function () {
+  if ($imageUrlInput.value !== '') {
+    imageUrls.push($imageUrlInput.value);
+    $imageUrlInput.value = '';
+    updateGallery();
+  }
+});
+
+function updateGallery() {
+  $gallery.innerHTML = '';
+  for (let i = 0; i < imageUrls.length; i++) {
+    const $imageElement = document.createElement('img');
+    $imageElement.className = 'gallery-image';
+    $imageElement.src = imageUrls[i];
+    $gallery.appendChild($imageElement);
+  }
+}
+
+updateGallery();
